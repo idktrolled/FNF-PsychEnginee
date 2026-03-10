b8bb83b954618b9d93f510174cd0b292e6b04bed
idktrolled <camilocarlos1100@gmail.com>
2026-03-10 10:02:23 -0500

    Update SUtil.hx

--- source/mobile/backend/SUtil.hx
+++ source/mobile/backend/SUtil.hx
@@ -4,8 +4,8 @@ import lime.system.System as LimeSystem;
 import haxe.io.Path;
 import haxe.Exception;
 #if android
-import extension.androidtools.Tools;
-import extension.androidtools.callback.CallBack;
+import android.Tools;
+import android.callback.CallBack;
 #end
 /**
  * A storage class for mobile.
